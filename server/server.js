ddb8dba9d569cce5bd090d59f17a19edcbede3da
Sander <sander.v@softtouch.be>
2024-02-29 13:09:50 +0100

    port server

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ||3000;
 
 // Use CORS middleware
 app.use(cors());
